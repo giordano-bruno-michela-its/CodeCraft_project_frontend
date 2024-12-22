55cbabc166c2b27cc3aa03e77d33aeb56762683e
gabrio993 <gabriele93.fusco@gmail.com>
2024-12-22 13:57:11 +0100

    feat: added interface in src/Types/tyes.ts

---
+++ CascinaCaccia/src/Types/types.ts
@@ -0,0 +1,9 @@
+export interface FormDataRequest {
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+  association: string;
+  activities: string[];
+  additionalInfo: string;
+}
