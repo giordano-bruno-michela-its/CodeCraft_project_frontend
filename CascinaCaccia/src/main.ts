cfff276b5e973f40eb33ee1e3160e88da86c34c2
gabrio993 <gabriele93.fusco@gmail.com>
2024-12-22 16:19:41 +0100

    feat: added the entry point of the application with the function calls

---
+++ CascinaCaccia/src/main.ts
@@ -0,0 +1,29 @@
+import { getFormData } from "./Components/bookingFormHandler";
+import { submitBookingForm } from "./Services/api";
+import { setupActivitiesToggle } from "./Utils/utils";
+
+// gettting html elements
+document.addEventListener("DOMContentLoaded", () => {
+  const infoForm = document.getElementById("infoForm") as HTMLFormElement;
+  const activitiesToggle = document.getElementById("activitiesToggle") as HTMLButtonElement;
+  const activitiesContainer = document.getElementById("activitiesContainer") as HTMLDivElement;
+
+  setupActivitiesToggle(activitiesToggle, activitiesContainer);
+
+  infoForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // get data from the html Form
+    const formData = getFormData(infoForm, activitiesContainer);
+
+    // Handling form data (submit)
+    try {
+      const result = await submitBookingForm(formData);
+      console.log("Dati inviati con successo:", result);
+      alert("La tua richiesta è stata inviata con successo!");
+    } catch (error) {
+      console.error("Errore durante l’invio dei dati:", error);
+      alert("Si è verificato un errore durante l’invio della tua richiesta. Riprova più tardi.");
+    }
+  });
+});
