61a39e62d016eaf7bc4870bb4f918e14081940cd
gabrio993 <gabriele93.fusco@gmail.com>
2024-12-22 13:15:20 +0100

    feat: added Activities Data Object in src/Components/activitiesData.ts

---
+++ CascinaCaccia/src/Components/activitiesData.ts
@@ -0,0 +1,28 @@
+import { ActivitiesByAgeGroup } from "../Types/types";
+
+// Group activities by age group
+
+export const activitiesByAgeGroup: ActivitiesByAgeGroup = {
+  Fascia1: ["3. Regolegalità", "6. Miele e Api"],
+  Fascia2: ["1. Storia della cascina Carla e Bruno Caccia", "2. Bruno Caccia", "3. Regolegalità", "6. Miele e Api", "9. Il gioco non è un azzardo"],
+  Fascia3: [
+    "1. Storia della cascina Carla e Bruno Caccia",
+    "2. Bruno Caccia",
+    "4. I beni confiscati",
+    "5. Le mafie e il cibo",
+    "6. Miele e Api",
+    "7. Ricicliamo attività artistiche e manuali",
+    "8. La mafia attraverso il cinema: percezioni culturali",
+    "9. Il gioco non è un azzardo",
+  ],
+  Fascia4: [
+    "1. Storia della cascina Carla e Bruno Caccia",
+    "2. Bruno Caccia",
+    "4. I beni confiscati",
+    "5. Le mafie e il cibo",
+    "6. Miele e Api",
+    "7. Ricicliamo attività artistiche e manuali",
+    "8. La mafia attraverso il cinema: percezioni culturali",
+    "10. Chi non gioca vince",
+  ],
+};
