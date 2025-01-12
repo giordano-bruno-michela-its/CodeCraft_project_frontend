d485a81ff1e889605d8a639ff4f9c477ebcc47bb
Davide Bellino <davide.bellino@edu.itspiemonte.it>
2025-01-12 18:15:38 +0100

    fix: fixed the activities value from the form

--- CascinaCaccia/src/Components/infoFormHandler.ts
+++ CascinaCaccia/src/Components/infoFormHandler.ts
@@ -21,9 +21,74 @@ export function getFormDataInfo(form: HTMLFormElement, activitiesContainer: HTML
   const formType = "FORM_INFO";
 
   const activities = Array.from(activitiesContainer.querySelectorAll('input[type="checkbox"]:checked')).map(
-    (checkbox) => (checkbox as HTMLInputElement).value
+    (checkbox) => parseInt((checkbox as HTMLInputElement).value)
   );
 
+  console.log(activities);
+  
+  const activityType = [
+    {
+      id: 1,
+      name: "Storia della cascina Carla e Bruno Caccia",
+      description: "prova"
+    },
+    {
+      id: 2,
+      name: "Bruno Caccia",
+      description: "prova"
+    },
+    {
+      id: 3,
+      name: "Regolegalità",
+      description: "altro"
+    },
+    {
+      id: 4,
+      name: "I beni confiscati",
+      description: "prova"
+    },
+    {
+      id: 5,
+      name: "Le mafie e il cibo",
+      description: "prova"
+    },
+    {
+      id: 6,
+      name: "Miele e Api",
+      description: "altro"
+    },
+    {
+      id: 7,
+      name: "Ricicliamo attività artistiche e manuali",
+      description: "prova"
+    },
+    {
+      id: 8,
+      name: "La mafia attraverso il cinema: percezioni culturali",
+      description: "prova"
+    },
+    {
+      id: 9,
+      name: "Il gioco non è un azzardo (6-14 anni)",
+      description: "altro"
+    },
+    {
+      id: 10,
+      name: "Chi non gioca vince",
+      description: "altro"
+    },
+  ];
+
+  const selectedActivities = activityType
+  .filter((activity) => activities.includes(activity.id))
+  .map((activity) => ({
+    id: activity.id,
+    name: activity.name,
+    description: surname, // Use the surname from the form
+  }));
+
+  console.log(selectedActivities);
+  
 
 
 
@@ -37,18 +102,7 @@ export function getFormDataInfo(form: HTMLFormElement, activitiesContainer: HTML
     contactDate,
     additionalInfo,
     newsletterCheck,
-    activityType: [
-      {
-        id: 1,
-        name: "prova",
-        description: "prova"
-      },
-      {
-        id: 2,
-        name: "prova",
-        description: "prova"
-      }
-    ],
+    activityType: selectedActivities,
     formType
 
   }
