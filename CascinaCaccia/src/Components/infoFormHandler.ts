b36497cfd0cdd4beab7ce0e784be79bc13e54b38
Davide Bellino <davide.bellino@edu.itspiemonte.it>
2025-01-13 22:07:16 +0100

    fix checkbox in information and booking forms and the value of the activity in infoFormHandler.ts

--- CascinaCaccia/src/Components/infoFormHandler.ts
+++ CascinaCaccia/src/Components/infoFormHandler.ts
@@ -29,54 +29,50 @@ export function getFormDataInfo(form: HTMLFormElement, activitiesContainer: HTML
   const activityType = [
     {
       id: 1,
-      name: "Storia della cascina Carla e Bruno Caccia",
-      description: "prova"
+      name: "STORIA DI CASCINA Carla e Bruno CACCIA",
+      description: "Un racconto avvincente della storia della Cascina e del suo impegno contro le mafie"
     },
     {
       id: 2,
-      name: "Bruno Caccia",
-      description: "prova"
+      name: "BRUNO CACCIA",
+      description: " Laboratorio sulla figura di Bruno Caccia sotto il profilo istituzionale e umano"
     },
     {
       id: 3,
-      name: "Regolegalità",
-      description: "altro"
+      name: "INTRODUZIONE AL TEMA MAFIA",
+      description: "Attività introduttiva volta a definire, conoscere e far conoscere il fenomeno mafioso"
     },
     {
       id: 4,
-      name: "I beni confiscati",
-      description: "prova"
+      name: "I BENI CONFISCATI",
+      description: "Attività di approfondimento sui beni confiscati, delle leggi sulla confisca dei beni e delle novità legislative"
     },
     {
       id: 5,
-      name: "Le mafie e il cibo",
-      description: "prova"
+      name: "LE MAFIE IN PIEMONTE",
+      description: " Attività di approfondimento sulla presenza delle mafie sul territorio piemontese"
     },
     {
       id: 6,
-      name: "Miele e Api",
-      description: "altro"
+      name: "SOSTENIBILITÀ AMBIENTALE",
+      description: "Approfondimenti sui temi della sostenibilità ambientale e della disuguaglianza con riflessioni sui problemi e la ricerca delle possibili soluzioni."
     },
     {
       id: 7,
-      name: "Ricicliamo attività artistiche e manuali",
-      description: "prova"
+      name: "LA MAFIA ATTRAVERSO IL CINEMA",
+      description: "Riflessioni sui modelli mafiosi presentati dal cinema per comprendere il loro impatto sulla società e migliorare la comprensione critica (e su quanto siano rappresentativi della realtà oggettiva e del sentire comune)."
     },
     {
       id: 8,
-      name: "La mafia attraverso il cinema: percezioni culturali",
-      description: "prova"
+      name: "IL GIOCO NON È UN AZZARDO (6-14 anni)",
+      description: "L'attività mira a esplorare le varie tipologie di gioco e le loro caratteristiche e a riflettere sulla correttezza dei giochi a cui si prende parte"
     },
     {
       id: 9,
-      name: "Il gioco non è un azzardo (6-14 anni)",
-      description: "altro"
+      name: "CHI NON GIOCA VINCE",
+      description: "Considerazioni per sensibilizzare ai rischi e alla dipendenza del gioco d'azzardo e ai collegamenti tra mafia e gioco d'azzardo illegale"
     },
-    {
-      id: 10,
-      name: "Chi non gioca vince",
-      description: "altro"
-    },
+  
   ];
 
   const selectedActivities = activityType
@@ -84,7 +80,7 @@ export function getFormDataInfo(form: HTMLFormElement, activitiesContainer: HTML
   .map((activity) => ({
     id: activity.id,
     name: activity.name,
-    description: surname, // Use the surname from the form
+    description: activity.description,
   }));
 
   console.log(selectedActivities);
@@ -92,7 +88,6 @@ export function getFormDataInfo(form: HTMLFormElement, activitiesContainer: HTML
 
 
 
-  // shorthand syntax: we can omit the value of the properties if they have the same name of the variable
   return {
     email,
     name,
