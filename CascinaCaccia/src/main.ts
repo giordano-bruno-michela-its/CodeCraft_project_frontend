408350111ad4e7c1630b0bfef7845e341d7bb172
Samuel Stocco <samuel.stocco@edu.itspiemonte.it>
2025-01-18 15:43:02 +0100

    fix: final, commit, moving to branch 71
    moving to branch 71 to finish carousel

--- CascinaCaccia/src/main.ts
+++ CascinaCaccia/src/main.ts
@@ -7,7 +7,7 @@ import { toggleDarkMode } from "./Utils/darkMode";
 import { setupActivitiesToggle, setupOfferCards } from "./Utils/utils";
 import { toggleAnswer } from "./Utils/utils";
 import { submitFormData } from "./Services/api";
-import {activityCards } from "./Utils/utils"
+import { activityCards } from "./Utils/utils"
 import { activityCarousel } from "./Utils/utils"
 import { getNewsletterFormData } from "./Components/newsletterFormHandler";
 
@@ -113,13 +113,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// carousel script
+// Swiper.js carousel script
 
 new Swiper(".activity-img-swiper", {
   modules: [Navigation, Pagination],
   slidesPerView: "auto",
   centeredSlides: true,
-  spaceBetween: 18,
+  spaceBetween: 16,
   loop: true,
   grabCursor: true,
   pagination: {
@@ -129,6 +129,15 @@ new Swiper(".activity-img-swiper", {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    500: {
+      spaceBetween: 24,
+    },
+    // 1024: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 16,
+    // },
   },
 });
 
