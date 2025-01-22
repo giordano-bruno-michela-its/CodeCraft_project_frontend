ee001f6b0d611a05c96f74f45516cac3f83d52de
gabrio993 <gabriele93.fusco@gmail.com>
2025-01-22 16:20:09 +0100

    added udate-booking.ts

---
+++ CascinaCaccia/src/Pages/update-booking.ts
@@ -0,0 +1,38 @@
+// Update booking form after retrieve reservation
+
+import { getUpdateBknFormData } from "../Components/getUpdateBookingFormData";
+import { prefillFields } from "../Utils/update-booking-reservation-prefill";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const reservationDetails = JSON.parse(localStorage.getItem("reservationDetails") || "{}");
+
+  prefillFields(reservationDetails);
+
+  const form = document.getElementById("bookingForm") as HTMLFormElement;
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const updatedData = getUpdateBknFormData();
+
+    try {
+      const URL = `http://localhost:8080/api/formreq/updatebooking/${reservationDetails.id}`;
+      const response = await fetch(URL, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      });
+
+      if (response.ok) {
+        alert("Prenotazione aggiornata con successo!");
+        form.reset();
+        window.location.href = "../../index.html";
+      } else {
+        throw new Error("Errore durante l'aggiornamento della prenotazione");
+      }
+    } catch (error) {
+      alert("Errore durante l'aggiornamento: ");
+    }
+  });
+});
--- CascinaCaccia/src/main.ts
+++ CascinaCaccia/src/main.ts
@@ -15,8 +15,6 @@ import { getNewsletterFormData } from "./Components/newsletterFormHandler";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import { getRetrieveReservationFormData } from "./Components/getRetrieveReservationFormData";
-import { prefillFields } from "./Utils/update-booking-reservation-prefill";
-import { getUpdateBknFormData } from "./Components/getUpdateBookingFormData";
 
 document.addEventListener("DOMContentLoaded", () => {
   //Navbar:
@@ -142,42 +140,6 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
       console.error("Errore durante l'invio dei dati", error);
       alert("Errore durante l'invio dei dati");
-    }
-  });
-});
-
-// Update booking form after retrieve reservation
-
-document.addEventListener("DOMContentLoaded", () => {
-  const reservationDetails = JSON.parse(localStorage.getItem("reservationDetails") || "{}");
-
-  prefillFields(reservationDetails);
-
-  const form = document.getElementById("bookingForm") as HTMLFormElement;
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const updatedData = getUpdateBknFormData();
-
-    try {
-      const URL = `http://localhost:8080/api/formreq/updatebooking/${reservationDetails.id}`;
-      const response = await fetch(URL, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedData),
-      });
-
-      if (response.ok) {
-        alert("Prenotazione aggiornata con successo!");
-        form.reset();
-        window.location.href = "../../index.html";
-      } else {
-        throw new Error("Errore durante l'aggiornamento della prenotazione");
-      }
-    } catch (error) {
-      alert("Errore durante l'aggiornamento: ");
     }
   });
 });
