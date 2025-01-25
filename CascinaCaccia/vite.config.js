5dfff71b3f77c8b5d213e9f11c3397ac3a71fcad
Davide Bellino <davide.bellino@edu.itspiemonte.it>
2025-01-25 16:31:22 +0100

    fix settings in vite.config.js

--- CascinaCaccia/vite.config.js
+++ CascinaCaccia/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "index.html",
+        backoffice: 'src/backoffice/index.html',
         retrieveReservation: "src/Pages/retrieve-reservation.html",
       },
       output: {
