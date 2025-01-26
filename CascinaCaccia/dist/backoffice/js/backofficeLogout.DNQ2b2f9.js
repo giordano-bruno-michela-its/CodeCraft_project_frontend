6d0adb310505c0b539bb3c8b8eaa4dd0790b3b07
Davide Bellino <davide.bellino@edu.itspiemonte.it>
2025-01-26 01:40:48 +0100

    add dist for testing

---
+++ CascinaCaccia/dist/backoffice/js/backofficeLogout.DNQ2b2f9.js
@@ -0,0 +1,1 @@
+import"../../js/modulepreload-polyfill.B5Qt9EMX.js";const t=sessionStorage.getItem("authToken");t?document.getElementById("logoutButton").addEventListener("click",()=>{sessionStorage.removeItem("authToken"),window.location.href="../Pages/login.html"}):window.location.href="../index.html";
